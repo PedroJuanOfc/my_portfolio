2c970b59ee9faa9f901182f42c25c0d3b22cbfbc
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-10-30 22:50:24 -0300

    feat(education): create Education section component

---
+++ src/components/sections/Education.tsx
@@ -0,0 +1,26 @@
+export default function Education() {
+  return (
+    <section id="education" className="space-y-4 py-16 border-t">
+      <h2 className="text-2xl font-semibold">Formação Acadêmica</h2>
+      <ul className="list-disc pl-6 text-muted-foreground space-y-1">
+        <li>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum
+          odio neque cupiditate provident placeat sed. Debitis maiores obcaecati
+          tempora eligendi. Magnam vero ducimus, unde quaerat in dolor vel
+          consectetur laborum!
+        </li>
+        <li>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda
+          animi vel amet, eum officia natus, excepturi quae laudantium molestias
+          fugiat totam alias. Tempore quo est quod excepturi sint iste possimus!
+        </li>
+        <li>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit
+          praesentium nisi facere possimus molestias officiis quo recusandae
+          vero itaque magni dolor eligendi tempore rem, voluptatum provident
+          libero eos suscipit deserunt?
+        </li>
+      </ul>
+    </section>
+  )
+}
