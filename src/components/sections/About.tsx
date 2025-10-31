40fe208d127d4c029488beff0a2f77f3f37d78ed
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-10-30 22:44:42 -0300

    feat(about): create About section component

---
+++ src/components/sections/About.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <section id="about" className="space-y-4 py-16 border-t">
+      <h2 className="text-2xl font-semibold">Sobre mim</h2>
+      <p className="text-muted-foreground leading-relaxed">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+        asperiores necessitatibus tenetur sunt fugit quam modi itaque totam qui,
+        facilis consequatur iusto suscipit, aliquid possimus nobis, aliquam
+        deleniti ullam pariatur?
+      </p>
+    </section>
+  )
+}
